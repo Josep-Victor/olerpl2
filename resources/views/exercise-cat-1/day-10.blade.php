777f83f756a50e3dffc322e75806d316d9a0e5cb
Josep Victor <josepvictorrrrr@gmail.com>
2021-01-25 17:26:59 +0700

    make it better

--- resources/views/exercise-cat-1/day-10.blade.php
+++ resources/views/exercise-cat-1/day-10.blade.php
@@ -1,6 +1,7 @@
-@extends('master-2')
+@extends('master')
     @section('extra-head')
-    <script type="text/javascript" src='/js/js_exercise-8.js'></script>
+        <script type="text/javascript" src='/js/js_exercise-8.js'></script>
+        <link rel="stylesheet" href='/css/app.css'>
     @endsection
     @section('konten')
         <section id="ole" class="day-one pb-5">
