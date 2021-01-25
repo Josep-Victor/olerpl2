130fe15c1f6eaf84b54f9d15f5861e66504cafdc
Josep Victor <josepvictorrrrr@gmail.com>
2021-01-25 15:14:24 +0700

    make it better

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://olahragaonline.herokuapp.com'),
+    'url' => env('APP_URL', 'http://olahragaonline.herokuapp.com'),
 
     'asset_url' => env('ASSET_URL', null),
 
